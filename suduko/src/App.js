535592ce7dcfae6125fad1b164e4817e388a7387
akash <akashdp9@gmail.com>
2019-11-30 10:48:58 +0530

    suduko

---
+++ suduko/src/App.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Box extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state =
+      {
+        count :1,
+        color: '#ccc',
+        disabled: false
+        
+      
+      };
+    
+  }
+  checkAnswer(useranswer){
+    if(this.props.answer !== useranswer && useranswer!=''){
+      if(this.state.count == 1)
+      {
+        this.setState(
+          {
+            color:'green',
+            count:2
+          }
+        )
+
+      }
+
+      if(this.state.count == 2)
+      {
+        this.setState(
+          {
+            color:'yellow',
+            count:3
+          }
+        )
+
+      }
+
+      if(this.state.count == 3)
+      {
+        this.setState(
+          {
+            color:'red',
+            count:4,
+            disabled :true
+          }
+        )
+
+      }
+   
+    }
+    else
+    {
+     if(this.props.answer === useranswer)
+     {
+      this.setState(
+        {
+         color:'blue',
+         disabled : true,
+         useranswer:this.state.answer
+        }
+        )
+     }
+    }
+
+  }
+
+
+
+
+ render(){
+      return (
+        <div className='grid-box'>
+          {this.props.number ? (
+            <div className='Box' > {this.props.number}</div>
+          ) : (
+
+                  <input type='text' value={this.state.value} style={{backgroundColor:this.state.color}} 
+                  onChange={(event)=> this.checkAnswer(event.target.value)}  disabled={(this.state.disabled)? 'disabled':''} maxLength={1} />
+                )
+          }
+        </div>
+      )
+
+
+    }
+}
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <div className="line">
+          <div className="Box1-row">
+
+            <Box number={3} />
+            <Box answer={1} />
+            <Box answer={4} />
+            <Box number={2} />
+          </div>
+        </div>
+        <div className="line">
+          <div className="Box1-row">
+            <Box answer={2} />
+            <Box number={4} />
+            <Box number={1} />
+            <Box answer={3} />
+
+          </div>
+        </div>
+        <div className="line">
+          <div className="Box1-row">
+            <Box answer={1} />
+            <Box number={3} />
+            <Box number={2} />
+            <Box answer={4} />
+
+          </div>
+        </div>
+        <div className="line">
+          <div className="Box1-row">
+            <Box number={4} />
+            <Box answer={2} />
+            <Box answer={3} />
+            <Box number={1} />
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default App;
+
+
